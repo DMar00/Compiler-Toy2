c15764672a53a9a010a5f39e2170073c02bf9928
chiaraOR <orrigochiara@gmail.com>
2024-02-20 11:54:57 +0100

    prova main

--- src/main/testers/Toy2ToC.java
+++ src/main/testers/Toy2ToC.java
@@ -27,29 +27,37 @@ public class Toy2ToC {
             /*XMLVisitor xmlGen = new XMLVisitor();
             astRoot.accept(xmlGen);*/
 
-            //SymbolTable symbolTable = new SymbolTable();
-            //1° visita
-            SemanticVisitorFirstVisit scopingVisitor = new SemanticVisitorFirstVisit();
-            astRoot.accept(scopingVisitor);
+            SemanticVisitorFirstVisit scopingVisitor = null;
+            SemanticVisitorSecondVisit scopingVisitor2 = null;
+            try{
+                //1° visita
+                /*SemanticVisitorFirstVisit */scopingVisitor = new SemanticVisitorFirstVisit();
+                astRoot.accept(scopingVisitor);
 
-            //2° visita
-            SemanticVisitorSecondVisit scopingVisitor2 = new SemanticVisitorSecondVisit(scopingVisitor.getActiveSymbolTable());
-            astRoot.accept(scopingVisitor2);
+                //2° visita
+                /*SemanticVisitorSecondVisit*/ scopingVisitor2 = new SemanticVisitorSecondVisit(scopingVisitor.getActiveSymbolTable());
+                astRoot.accept(scopingVisitor2);
+            }catch (RuntimeException e){
+                System.err.println(e);
+            }finally {
+                //Visitor C
+                CVisitor cVisitor = new CVisitor(scopingVisitor.getFuncMap(), scopingVisitor.getProcMap());
+                astRoot.accept(cVisitor);
 
-            //Visitor C
-            CVisitor cVisitor = new CVisitor(scopingVisitor.getFuncMap(), scopingVisitor.getProcMap());
-            astRoot.accept(cVisitor);
+                Path percorso = Paths.get(args[0]);
+                String fileName = percorso.getFileName().toString();
+                String cFileName = fileName.substring(0,fileName.length()-4) ;
+                String cFileNamePlusExtension = cFileName+".c";
+                //System.out.println("test_files"+File.separator+"c_out"+File.separator+cFileNamePlusExtension);
+                cVisitor.printToFile("c_out"+File.separator+cFileNamePlusExtension);
+            }
 
-            Path percorso = Paths.get(args[0]);
-            String fileName = percorso.getFileName().toString();
-            String cFileName = fileName.substring(0,fileName.length()-4) ;
-            String cFileNamePlusExtension = cFileName+".c";
-            //System.out.println("test_files"+File.separator+"c_out"+File.separator+cFileNamePlusExtension);
-            cVisitor.printToFile("c_out"+File.separator+cFileNamePlusExtension);
+
+
 
 
             //ESECUZIONE DI GCC PER CREARE IL FILE OUTPUT.EXE A PARTIRE DAL FILE.C
-            ProcessBuilder builder = new ProcessBuilder("gcc", "-o", cFileName+".exe", cFileNamePlusExtension);
+            /*ProcessBuilder builder = new ProcessBuilder("gcc", "-o", cFileName+".exe", cFileNamePlusExtension);
             builder.directory(new File("c_out"));
             builder.redirectErrorStream(true);
             Process p = builder.start();
@@ -60,9 +68,9 @@ public class Toy2ToC {
                     String line = sc.nextLine();
                     System.out.println(line);
                 }
-            }
+            }/*
 
-            /*//ESECUZIONE E STAMPA DEL FILE OUTPUT.EXE
+            /*ESECUZIONE E STAMPA DEL FILE OUTPUT.EXE
             ProcessBuilder processBuilder = new ProcessBuilder("c_out"+File.separator+cFileName+".exe");
             Process process = processBuilder.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -72,14 +80,15 @@ public class Toy2ToC {
             }*/
 
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println(e);
+            //e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         try {
             main2(args);
-        } catch(Throwable e) {
+        } catch(Exception e) {
             System.err.println(e);
         }
     }
